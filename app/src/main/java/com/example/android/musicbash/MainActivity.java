9cc98011672991819a2af37345a635e88203e7b5
SrikantKarnani <srikant.karnani352@gmail.com>
2017-06-20 18:45:45 +0530

    Version 2

--- app/src/main/java/com/example/android/musicbash/MainActivity.java
+++ app/src/main/java/com/example/android/musicbash/MainActivity.java
@@ -21,6 +21,7 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 
@@ -66,6 +67,8 @@ import static android.R.attr.name;
 import static android.R.attr.permission;
 import static android.R.attr.theme;
 import static android.media.CamcorderProfile.get;
+import static com.example.android.musicbash.R.layout.fragment_blank;
+
 public class MainActivity extends AppCompatActivity {
     private static final int REQUEST_READABLE_CODE = 144 ;
     private static final int REQUEST_PERMISSION_WRITE = 1001;
@@ -82,6 +85,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         while (!checkPermissions()){};
         setContentView(R.layout.activity_main);
+        Fragment fragy = BlankFragment.newInstance();
+        getSupportFragmentManager().beginTransaction().add(R.id.frag_container,fragy).commit();
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.myFAB);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -99,38 +104,7 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayShowTitleEnabled(true);
         getSupportActionBar().setTitle(R.string.toolbarT);
         getSupportActionBar().setSubtitle(R.string.toolbatST);
-        lv = (RecyclerView) findViewById(R.id.lvPlaylist);
-        String selection = MediaStore.Audio.Media.IS_MUSIC + "!= 0";
-        String[] projection = {
-                MediaStore.Audio.Media._ID,
-                MediaStore.Audio.Media.TITLE,
-                MediaStore.Audio.Media.ALBUM,
-                MediaStore.Audio.Media.DATA,
-                MediaStore.Audio.Media.DURATION,
-                MediaStore.Audio.Media.ALBUM_ID
-        };
-        String orderBy = MediaStore.Audio.Media.TITLE + " ASC";
-                cursor = this.managedQuery(
-                        MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
-                        projection,
-                        selection,
-                        null,
-                        orderBy
-                );
 
-        songsObject = new ArrayList<>();
-        addSongs(cursor);
-//        Comparator comp = new Comparator<String>() {
-//            @Override
-//            public int compare(String o1, String o2) {
-//                return o1.toLowerCase().compareTo(o2.toLowerCase());
-//            }
-//        };
-//        Collections.sort(songsName, comp);
-
-        ar = new recylcerAdapter(this, songsObject);
-        lv.setAdapter(ar);
-        lv.setNestedScrollingEnabled(false);
     }
         /* Checks if external storage is available for read and write */
         public boolean isExternalStorageWritable() {
@@ -204,49 +178,6 @@ public class MainActivity extends AppCompatActivity {
                 new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
                 REQUEST_READABLE_CODE);
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main_menu, menu);
-        SearchView searchView1 = (SearchView) menu.findItem(R.id.search_bar).getActionView();
-        final SearchManager searchManager = (SearchManager) getSystemService(SEARCH_SERVICE);
-        searchView1.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
-        searchView1.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-                return false;
-            }
-
-            @Override
-            public boolean onQueryTextChange(String newText) {
-                newText = newText.toLowerCase();
-                List<songs> newList = new ArrayList<>();
-                for(songs name : songsObject){
-                    if((name.getTitle().toLowerCase()).contains(newText)){
-                        newList.add(name);
-                    }
-                }
-                ar.filter(newList);
-                return true;
-            }
-        });
-        return super.onCreateOptionsMenu(menu);
-    }
-    void addSongs(Cursor cur){
-        cur.moveToFirst();
-        do {
-            songsObject.add(new songs(cur.getString(cur.getColumnIndex(MediaStore.Audio.Media._ID)),
-                    cur.getString(cur.getColumnIndex(MediaStore.Audio.Media.TITLE)),
-                    cur.getString(cur.getColumnIndex(MediaStore.Audio.Media.ALBUM)),
-                    cur.getString(cur.getColumnIndex(MediaStore.Audio.Media.DATA)),
-                    cur.getString(cur.getColumnIndex(MediaStore.Audio.Media.DURATION)),
-                    cur.getString(cur.getColumnIndex(MediaStore.Audio.Media.ALBUM_ID))
-            ));
-        }while (cur.moveToNext());
-
-
-    }
-
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
@@ -262,7 +193,12 @@ public class MainActivity extends AppCompatActivity {
             case R.id.importJSON:
                 break;
             case R.id.sortDate:
-
+                Fragment last = lastAdded.newInstance();
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.frag_container,last)
+                        .addToBackStack(null)
+                        .commit();
         }
         return super.onOptionsItemSelected(item);
     }
